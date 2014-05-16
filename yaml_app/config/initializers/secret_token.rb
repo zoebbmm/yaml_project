fcb3e2fb2cb59e5d904ba1d25dc8377cc842af47
Wei Zhou <zoebbmm@gmail.com>
2014-05-16 12:47:32 -0400

    initialize Repository

---
+++ yaml_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YamlApp::Application.config.secret_key_base = '63c968d178e3780ab1a56cce6e4c1a20fa19cbf0fb45dca6637015f715decda1440174381c6fb4d6d66f41ee41663a90f927a2c817ccd8730e55fe686b1bb2fd'
